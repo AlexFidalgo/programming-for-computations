c78df62a81321a23a53ae93af5ace2c34aa09cfa
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-04-27 14:55:28 -0300

    Create trapezoidal.py

---
+++ Computing Integrals/trapezoidal.py
@@ -0,0 +1,21 @@
+from math import exp
+
+def trapezoidal(f, a, b, n):
+    h = float(b-a)/n
+    result = 0.5*f(a) + 0.5*f(b)
+    for i in range(1, n):
+        result += f(a + i*h)
+    result *= h
+    return result
+
+v = lambda t: 3*(t**2)*exp(t**3)
+n = 4
+
+numerical = trapezoidal(v, 0, 1, n)
+
+V = lambda t: exp(t**3)
+exact = V(1) - V(0)
+
+error = exact - numerical
+
+print('n=%d: %.16f, error: %g' % (n, numerical, error))
