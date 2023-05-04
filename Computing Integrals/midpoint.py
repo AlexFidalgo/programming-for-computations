29c5df28cafe5fe80b51e185582a301372f4aa4c
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-05-04 13:19:22 -0300

    vectorizes code

--- Computing Integrals/midpoint.py
+++ Computing Integrals/midpoint.py
@@ -1,6 +1,7 @@
-from math import exp
+from numpy import exp
+from numpy import linspace, sum
 
-def midpoint(f, a, b, n):
+def midpoint_without_vectorization(f, a, b, n):
     h = float(b-a)/n
     result = 0
     for i in range(n):
@@ -8,16 +9,25 @@ def midpoint(f, a, b, n):
     result *= h
     return result
 
+def midpoint_vectorized(f, a, b, n):
+    h = float(b-a)/n
+    x = linspace(a + h/2, b - h/2, n)
+    return h*sum(f(x))
+
 if __name__ == '__main__':
 
-    v = lambda t: 3*(t**2)*exp(t**3)
+    #Note the necessity to use exp from numpy: our v function will be called with x as an array, and the exp function must be capable of 
+    # working with an array.
+    v = lambda t: 3*t**2*exp(t**3) 
     n = 4
 
-    numerical = midpoint(v, 0, 1, n)
+    numerical = midpoint_without_vectorization(v, 0, 1, n)
+    
+    numerical_vectorized = midpoint_vectorized(v, 0, 1, n)
 
     V = lambda t: exp(t**3)
     exact = V(1) - V(0)
 
     error = exact - numerical
 
-    print('n=%d: %.16f, error: %g' % (n, numerical, error))
+    print('n=%d: numerical=%.16f, numerical_vectorized=%.16f, error: %g' % (n, numerical, numerical_vectorized, error))
