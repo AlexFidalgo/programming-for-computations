e13fdd0aa4d0f3e7dd5ee8c3f84421942af6bd7d
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-05-04 13:19:45 -0300

    vectorizes code

--- Computing Integrals/trapezoidal.py
+++ Computing Integrals/trapezoidal.py
@@ -1,4 +1,5 @@
-from math import exp
+from numpy import exp
+from numpy import linspace
 
 def trapezoidal(f, a, b, n):
     h = float(b-a)/n
@@ -8,16 +9,24 @@ def trapezoidal(f, a, b, n):
     result *= h
     return result
 
+def trapezoidal_vectorized(f, a, b, n):
+    h = float(b-a)/n
+    x = linspace(a, b, n+1)
+    s = sum(f(x)) - 0.5*f(a) - 0.5*f(b)
+    return h*s
+
 if __name__ == '__main__':
 
     v = lambda t: 3*(t**2)*exp(t**3)
     n = 4
 
     numerical = trapezoidal(v, 0, 1, n)
+    
+    numerical_vectorized = trapezoidal_vectorized(v, 0, 1, n)
 
     V = lambda t: exp(t**3)
     exact = V(1) - V(0)
 
     error = exact - numerical
 
-    print('n=%d: %.16f, error: %g' % (n, numerical, error))
+    print('n=%d: numerical=%.16f, numerical_vectorized=%.16f, error: %g' % (n, numerical, numerical_vectorized, error))
